460571394e2a1b1955bf85bf5d4ca89a2b7395d6
levinhax <levin1229@163.com>
2021-08-27 18:00:08 +0800

    feat: gitignore

--- dist/static/js/4.2455bf41.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp_qiankun-micro-react"]=this["webpackJsonp_qiankun-micro-react"]||[]).push([[4],{38:function(t,a,c){"use strict";c.r(a);c(0);var n=c(3);a.default=function(){return Object(n.jsx)("div",{className:"about-wrapper",children:"About"})}}}]);
-//# sourceMappingURL=4.2455bf41.chunk.js.map
