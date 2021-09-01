ea9973752c40ee1a4940705885eb9a26f188ce4e
levinhax <levin1229@163.com>
2021-09-01 19:40:57 +0800

    feat: echarts图表

--- src/components/lineChart/index.tsx
+++ src/components/lineChart/index.tsx
@@ -1,38 +1,163 @@
 import React, { useEffect, useRef } from 'react'
 import { IProps } from './type'
-import * as echarts from 'echarts'
+import * as echarts from 'echarts/core'
+import { LineChart, LineSeriesOption } from 'echarts/charts' // 系列类型的定义后缀都为 SeriesOption
+import {
+  TitleComponent,
+  // 组件类型的定义后缀都为 ComponentOption
+  TitleComponentOption,
+  TooltipComponent,
+  TooltipComponentOption,
+  GridComponent,
+  GridComponentOption,
+  LegendComponent,
+  LegendComponentOption,
+  MarkPointComponent,
+  MarkPointComponentOption,
+  MarkLineComponent,
+  MarkLineComponentOption,
+  ToolboxComponent,
+  ToolboxComponentOption,
+} from 'echarts/components'
+import { CanvasRenderer } from 'echarts/renderers'
 
-const LineChart: React.FC<IProps> = props => {
-  console.log('props: ', props)
+// 通过 ComposeOption 来组合出一个只有必须组件和图表的 Option 类型
+type ECOption = echarts.ComposeOption<
+  | LineSeriesOption
+  | TitleComponentOption
+  | TooltipComponentOption
+  | GridComponentOption
+  | LegendComponentOption
+  | MarkPointComponentOption
+  | MarkLineComponentOption
+  | ToolboxComponentOption
+>
+
+// 注册必须的组件
+echarts.use([
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  LegendComponent,
+  MarkPointComponent,
+  MarkLineComponent,
+  ToolboxComponent,
+  LineChart,
+  CanvasRenderer,
+])
+
+const ComLineChart: React.FC<IProps> = props => {
   const chartRef: any = useRef() //拿到DOM容器
+  let myChart: any = null
 
-  // 每当props改变的时候就会实时重新渲染
-  useEffect(() => {
-    const chart = echarts.init(chartRef.current) //echart初始化容器
-    let option = {
-      //配置项(数据都来自于props)
+  const renderChart = () => {
+    const option: ECOption = {
       title: {
-        text: '暂无数据',
+        text: '未来一周气温变化',
+        subtext: '纯属虚构',
+      },
+      tooltip: {
+        trigger: 'axis',
+      },
+      legend: {
+        show: true,
+        data: ['最高气温', '最低气温'],
+        // right: 60,
+        // top: 8,
+      },
+      toolbox: {
+        show: true,
+        feature: {
+          dataZoom: {
+            yAxisIndex: 'none',
+          },
+          dataView: { readOnly: false },
+          magicType: { type: ['line', 'bar'] },
+          restore: {},
+          saveAsImage: {},
+        },
       },
       xAxis: {
         type: 'category',
-        data: ['2021/08/20', '2021/08/21', '2021/08/22', '2021/08/23', '2021/08/24', '2021/08/25'],
+        boundaryGap: false,
+        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
       },
       yAxis: {
         type: 'value',
+        axisLabel: {
+          formatter: '{value} °C',
+        },
       },
       series: [
         {
-          data: [22, 19, 88, 66, 5, 90],
+          name: '最高气温',
           type: 'line',
+          data: props.series1Data,
+          markPoint: {
+            data: [
+              { type: 'max', name: '最大值' },
+              { type: 'min', name: '最小值' },
+            ],
+          },
+          markLine: {
+            data: [{ type: 'average', name: '平均值' }],
+          },
         },
+        {
+          name: '最低气温',
+          type: 'line',
+          data: props.series2Data,
+          markPoint: {
+            data: [{ name: '周最低', value: -2, xAxis: 1, yAxis: -1.5 }],
+          },
+          markLine: {
+            data: [
+              { type: 'average', name: '平均值' },
+              [
+                {
+                  symbol: 'none',
+                  x: '90%',
+                  yAxis: 'max',
+                },
+                {
+                  symbol: 'circle',
+                  label: {
+                    position: 'start',
+                    formatter: '最大值',
+                  },
+                  type: 'max',
+                  name: '最高点',
+                },
+              ],
+            ],
+          },
+        },
+        // blendMode: 'luminosity' // 柱状图重合的颜色，blendMode 混合模式
       ],
     }
 
-    chart.setOption(option)
+    myChart = echarts.init(chartRef.current) // echart初始化容器
+    myChart.setOption(option)
+  }
+
+  // 每当props改变的时候就会实时重新渲染
+  useEffect(() => {
+    renderChart()
+  }, [props])
+
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      handleResize()
+    })
   }, [])
 
-  return <div ref={chartRef} className="chart"></div>
+  const handleResize = () => {
+    if (myChart) {
+      myChart.resize()
+    }
+  }
+
+  return <div ref={chartRef} style={{ width: '100%', height: '100%' }}></div>
 }
 
-export default LineChart
+export default ComLineChart
--- src/views/Charts/lineChart.tsx
+++ src/views/Charts/lineChart.tsx
@@ -1,40 +1,37 @@
-import React, { useRef, useEffect } from 'react'
-import * as echarts from 'echarts'
-import './index.css'
+import React, { useState } from 'react'
+import ComLineChart from '../../components/lineChart'
+import './chart.css'
 
 function LineChartView() {
-  const chartRef: any = useRef()
+  const [chartData, setChartData] = useState({
+    title: '未来一周气温变化',
+    xData: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+    series1Data: [10, 11, 13, 11, 12, 12, 9],
+    series2Data: [1, -2, 2, 5, 3, 2, 0],
+  })
 
-  useEffect(() => {
-    const chart = echarts.init(chartRef.current) // echarts初始化容器
-    let option = {
-      //配置项(数据都来自于props)
-      title: {
-        text: '暂无数据',
-      },
-      xAxis: {
-        type: 'category',
-        data: ['2021/08/20', '2021/08/21', '2021/08/22', '2021/08/23', '2021/08/24', '2021/08/25'],
-      },
-      yAxis: {
-        type: 'value',
-      },
-      series: [
-        {
-          data: [22, 19, 88, 66, 15, 90],
-          type: 'line',
-        },
-      ],
-    }
-
-    chart.setOption(option)
-  }, [])
+  const handleUpdateData = () => {
+    setChartData({
+      title: '未来一周气温变化',
+      xData: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+      series1Data: [10, 10, 15, 16, 12, 12, 9],
+      series2Data: [1, -2, 2, 5, 5, 2, 0],
+    })
+  }
 
   return (
-    <div className="line-chart-wrapper">
-      <h2>LineChart</h2>
-      <div style={{ width: 360, height: 200 }}>
-        <div ref={chartRef} className="chart"></div>
+    <div className="chart-wrapper line-chart-wrapper">
+      <h2 className="title">未来一周气温变化(折线图)</h2>
+      <div className="chart-container">
+        <ComLineChart
+          title={chartData.title}
+          xData={chartData.xData}
+          series1Data={chartData.series1Data}
+          series2Data={chartData.series2Data}
+        />
+      </div>
+      <div className="btn-container">
+        <span onClick={handleUpdateData}>更新数据</span>
       </div>
     </div>
   )
